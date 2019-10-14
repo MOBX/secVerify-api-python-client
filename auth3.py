6a2236892e0fdb62fc619fb3be473ac2686e2338
tangwch <tangwch@yoozoo.com>
2019-10-14 16:11:44 +0800

    python3 demo

--- auth3.py
+++ auth3.py
@@ -8,8 +8,8 @@ from pyDes import *
 
 url = "http://identify.verify.mob.com/auth/auth/sdkClientFreeLogin"
 
-appkey = "moba6b6c6d6"
-appSecret = "b89d2427a3bc7ad1aea1e1e8c1d36bf3"
+appkey = ""
+appSecret = ""
 token = "59616292321333248"
 opToken = "opToken"
 operator = "CUCC"
